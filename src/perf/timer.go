d94b917e82b328ee0754fa5f80a334565fba7684
Braxton Plaxco <bplaxco@redhat.com>
2026-09-14 11:59:57 -0400

    Add start/end logs for perf timers to help detect fails
    rh-pre-commit.version: 2.4.0
    rh-pre-commit.check-secrets: ENABLED

--- src/perf/timer.go
+++ src/perf/timer.go
@@ -10,8 +10,10 @@ import (
 // "name" should be formatted LikeAClassName
 func Timer(name string) func() {
 	start := time.Now()
+	logging.Info("%sTimer: start=%d", name, start.Unix())
 
 	return func() {
-		logging.Info("%sTimer: duration=%v", name, time.Since(start))
+		end := time.Now()
+		logging.Info("%sTimer: end=%d duration=%v", name, end.Now(), end.Sub(start))
 	}
 }
